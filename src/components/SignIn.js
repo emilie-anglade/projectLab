cb21ac9162fa73d36e2f4540ab55adc28158bdf0
Léa Miquel <leamiquel21@gmail.com>
2021-01-14 17:20:27 +0100

    css home, sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -10,56 +11,69 @@ class SignIn extends React.Component {
     }
   }
 
-  handleInputChange = (event) => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }; 
-
   submitForm = (e) => {
     e.preventDefault();
     console.log(this.state);
     this.props.history.push('/profile');
   };
+render() {
 
-  render() {
-    return (
-      <div>
-        <h1>Connexion</h1>
-        <form onSubmit={this.submitForm}>
-          <label htmlFor="email">
-            E-mail :
-            <input
-              id="email"
-              name="email"
-              type="email"
-              required
-              checked={this.state.email}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label htmlFor="password">
-            Mot de passe :
-            <input
-              id="password"
-              name="password"
-              type="password"
-              minLength="6"
-              required
-              checked={this.state.password}
-              onChange={this.handleInputChange} />
-          </label>        
-          <br />
-          <input type="submit" value="Valider"/>
-        </form>
-      </div>
-  
-    )
+  const form = {
+    "margin-left": "auto",
+    "margin-right": "auto",
+    "margin-top": "10em",
+    "text-align": "center",
+    width: "50%",
+    border: "1px solid rgba(0,0,0,.125)",
+    "border-radius": "0.5em",
+    "box-shadow": "0px 4px 4px rgba(0,0,0,.125)"
   }
-}
+
+  const formContainer = {
+    "margin-left": "auto",
+    "margin-right": "auto",
+    "margin-top": "1em",
+    "margin-bottom": "1em",
+    width: "80%",
+    height: "80%"
+  }
+
+  const sizeText = {
+    "font-size": "18px"
+  }
+
+return (
+  <Form style={form} onSubmit={this.submitForm}>
+    <div style={formContainer}>
+    <h3>Connexion</h3>
+    <br/>
+    <Row form>
+      <Col>
+      <FormGroup>
+      <Label for="email" style={sizeText}>Email</Label>
+      <Input 
+        type="email" 
+        name="email" 
+        id="email"
+        required
+      />
+    </FormGroup>
+    <FormGroup>
+      <Label for="password" style={sizeText}>Mot de passe</Label>
+      <Input 
+        type="password" 
+        name="password" id="password" 
+        minLength="6"
+        required
+      />
+    </FormGroup>
+      </Col>
+    </Row>
+        <br/>
+    <Button>Valider</Button>
+    </div>
+  </Form>
+);
+}}
 
 export default withRouter(SignIn);
