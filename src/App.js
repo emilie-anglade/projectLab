5d2e161535ea34b592685fd49ba2411e2d63abf0
Emilie Anglade <anglade.em@gmail.com>
2021-01-14 10:25:36 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Connexion from './components/Connexion';
 import Home from './components/Home';
+import Inscription from './components/Inscription';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/connexion">
+            <Connexion />
+          </Route>
+          <Route path="/inscription">
+            <Inscription />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
