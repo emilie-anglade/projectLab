0fb63acba26da99c9d97012cfcef76c7acd16a6f
Emilie Anglade <anglade.em@gmail.com>
2021-01-14 09:49:33 +0100

    home

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to ProjectLab</h1>
+      <button>Inscription</button>
+      <button>Connexion</button>
+    </div>
+  )
+}
+
+export default Home;
