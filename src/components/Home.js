422e43d97fca95acfddc56ca46265da2ca11c7a7
Louisemehouas <mehouaslouise0@gmail.com>
2021-01-15 11:16:50 +0100

    Home try hard

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 import { Button } from "reactstrap";
 import logo from "./img/proje-lab.svg";
 import collab from './img/collab.svg';
+import { assertUserWhitespacable } from '@babel/types';
+
 
 const Home = () => {
 
@@ -15,11 +17,20 @@ const Home = () => {
     textAlign: "center",
     marginLeft: "auto",
     marginRight: "auto",
-    marginTop: "12em",
-    width: "70%",
+    marginTop: "5em",
+    width: "40%",
     border: "1px solid rgba(0,0,0,.125)",
     "border-radius": "0.5em",
-    "box-shadow": "0px 4px 4px rgba(0,0,0,.125)"
+    "box-shadow": "0px 4px 4px rgba(0,0,0,.125)",
+    "background-color": "#FFFFFF",
+    "opacity":"100%",
+    "object-fit":"cover",
+  }
+
+  const bodyHome = {
+    "width":"100%",
+    "opacity": "100%",
+    "height":"100%",
   }
 
   const title = {
@@ -28,18 +39,29 @@ const Home = () => {
   }
 
   const buttonHome = {
-    marginTop: "4em",
-    marginBottom: "4em",
-    width: "16em"
+    marginTop: "1em",
+    marginBottom: "2em",
+    width: "6em",
+
   }
 
   const flex = {
     display: "flex",
     "flex-wrap": "wrap",
-    "justify-content": "space-evenly"
+    "justify-content": "space-evenly",
+
   }
 
+  const divHome = {
+    "height": "100%",
+    "width": "100%",
+    "background-image":"url(http://marc-andre-dubout.org/vigie/technique/machine/plan/plan06.jpg)"
+  }
+
+
   return (
+  <body style= {bodyHome}>  
+   <div style ={divHome}>
     <div style={home}>
       <h1 style={title}><img src={logo} alt="logo" /></h1>
       <section style={flex}>
@@ -51,6 +73,8 @@ const Home = () => {
       </Link>
       </section>
     </div>
+   </div> 
+  </body>  
   )
 }
 
